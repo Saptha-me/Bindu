b58d86b17f654197a622d5a15caa35264e233aae
raahulmona <157174139+raahulmona@users.noreply.github.com>
2025-05-11 19:52:21 +0200

    feat: add Act, Listen, View protocol methods and reorganize agno examples

--- pebbling/core/protocol.py
+++ pebbling/core/protocol.py
@@ -14,6 +14,10 @@ from typing import Dict, Any, Optional, Union
 class ProtocolMethod(str, Enum):
     """Supported protocol methods"""
     CONTEXT = "Context"
+    ACT = "Act"
+    LISTEN = "Listen"
+    VIEW = "View"
+    
 
 
 class TaskStatus(str, Enum):

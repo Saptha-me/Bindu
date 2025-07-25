3cf100e4c74349c3fc302d032c09bbebf1407757
raahulmona <dutta.raahul@gmail.com>
2025-07-25 14:25:16 +0200

    refactor: move keys directory to pki and update security configuration

--- examples/example.py
+++ examples/example.py
@@ -1,16 +1,21 @@
 from pebbling.protocol import PebblingMessage, PebblingContext
 from pebbling.agent.pebblify import pebblify
-from pebbling.security.setup_security import setup_security
-from pebbling.registry.setup_registry import setup_registry
-from pebbling.deployment.setup_deployment import DeploymentConfig
+from pebbling.security.setup_security import create_security_config
+from pebbling.registry.setup_registry import create_registry_config
+from pebbling.security.ca.setup_ca import create_ca_config
+from pebbling.deployment.setup_deployment import create_deployment_config
+
+from agno import Agent
+from agno.openai import OpenAIChat
 
 
 @pebblify(
     name="News Reporter",
     description="Reports news with flair",
-    security=setup_security(did_required=True),
-    registry=setup_registry(store_in_hibiscus=True),
-    deployment=DeploymentConfig(expose=True, port=3773)
+    security=create_security_config(did_required=True, keys_required=True, recreate_keys=True),
+    registry=create_registry_config(store_in_hibiscus=True),
+    ca=create_ca_config(cert_authority="sheldon", issue_certificate=True),
+    deployment=create_deployment_config(expose=True, port=3773)
 )
 async def news_reporter(
     input: PebblingMessage, 

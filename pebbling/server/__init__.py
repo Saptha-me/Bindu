5ffeb49f54f910975627056aa430ee582bfb96be
raahulmona <dutta.raahul@gmail.com>
2025-09-07 16:54:00 +0200

    refactor: switch from story_agent to simple_agent implementation and remove Sheldon CA module

--- pebbling/server/__init__.py
+++ pebbling/server/__init__.py
@@ -18,7 +18,7 @@ protocols with shared task management and session contexts.
 from .applications import PebbleApplication
 from .storage import InMemoryStorage, PostgreSQLStorage, QdrantStorage
 from .scheduler import InMemoryScheduler, RedisScheduler
-from .workers import Worker, ManifestWorker
+from .workers import ManifestWorker
 from .task_manager import TaskManager
 
 __all__ = [
@@ -28,7 +28,6 @@ __all__ = [
     "QdrantStorage",
     "InMemoryScheduler",
     "RedisScheduler",
-    "Worker",
     "ManifestWorker",
     "TaskManager",
 ]

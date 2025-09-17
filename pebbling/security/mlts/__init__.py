721eb1d19c615f6101cd6230e7dbd33255df87de
Anikait <anikait.makkar@outlook.com>
2025-09-17 16:26:43 +0530

    chore: code cleanup, remove unused code files

--- pebbling/security/mlts/__init__.py
+++
@@ -1,5 +0,0 @@
-"""MLTS (Multi-Layer Trust System) security package for Pebbling.
-
-This package implements multi-layer trust mechanisms for secure agent
-communications and verification within the Pebbling framework.
-"""

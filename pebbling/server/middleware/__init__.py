84524dd248c1f4f7dc2edb3244d60628d2d7f0da
Anikait <anikait.makkar@outlook.com>
2025-10-01 23:17:03 +0530

    chore: remove unnecessary comments'

--- pebbling/server/middleware/__init__.py
+++ pebbling/server/middleware/__init__.py
@@ -1,13 +1,3 @@
-#
-# |---------------------------------------------------------|
-# |                                                         |
-# |                 Give Feedback / Get Help                |
-# | https://github.com/Pebbling-ai/pebble/issues/new/choose |
-# |                                                         |
-# |---------------------------------------------------------|
-#
-#  Thank you users! We ❤️ you! - 🐧
-
 """Middleware components for Pebbling server."""
 
 from .auth import AuthenticationMiddleware
